2b7cf53554e1eb74072b59799092c25e8285a402
persorkki <herroo@protonmail.com>
2020-04-03 23:07:42 +0300

    indent

--- bat-rises.py
+++ bat-rises.py
@@ -10,27 +10,27 @@ remote_working_dir = r"D:\Code\wip\bat-rises\test\networkdrive"
 local_working_dir = r"D:\Code\wip\bat-rises\test\local\DESKTOP"
 
 def read_files_txt():
-	with open("./test/testfiles.txt", "r") as f:
-		lines = [x.strip("\n") for x in f.readlines()]
-	return [x for x in lines if x.strip() and not x.startswith("#")]
+    with open("./test/testfiles.txt", "r") as f:
+        lines = [x.strip("\n") for x in f.readlines()]
+    return [x for x in lines if x.strip() and not x.startswith("#")]
 
 #TODO: rename push
 def dev_push(loglist, local_working_dir, remote_working_dir):
-	for path in loglist:
-		(relative_path, file) = os.path.split(path)
-		devs.do_path_push(relative_path, file, local_working_dir, remote_working_dir)
+    for path in loglist:
+        (relative_path, file) = os.path.split(path)
+        devs.do_path_push(relative_path, file, local_working_dir, remote_working_dir)
 
 #TODO: rename pull
 def dev_pull(loglist, local_working_dir, remote_working_dir):
-	for path in loglist:
-		(relative_path, file) = os.path.split(path)
-		devs.do_path_pull(relative_path, file, remote_working_dir, local_working_dir)
+    for path in loglist:
+        (relative_path, file) = os.path.split(path)
+        devs.do_path_pull(relative_path, file, remote_working_dir, local_working_dir)
 
 if __name__ == "__main__":
-	options = {"push":dev_push, "pull":dev_pull, "clogs": logs.create_logs}
+    options = {"push":dev_push, "pull":dev_pull, "clogs": logs.create_logs}
 
-	if not os.path.isfile("logs.json"):
-		logs.create_logs()
+    if not os.path.isfile("logs.json"):
+        logs.create_logs()
 
-	if len(sys.argv) > 1 and sys.argv[1].lower() in options:
-		options[sys.argv[1].lower()](loglist = read_files_txt(), local_working_dir=local_working_dir, remote_working_dir=remote_working_dir)
+    if len(sys.argv) > 1 and sys.argv[1].lower() in options:
+        options[sys.argv[1].lower()](loglist = read_files_txt(), local_working_dir=local_working_dir, remote_working_dir=remote_working_dir)
--- logs.py
+++ logs.py
@@ -3,26 +3,26 @@ import os
 import json
 
 def we_modified(dst, mtime):
-	dump = _read_logs()
-	return dump[dst] == mtime
+    dump = _read_logs()
+    return dump[dst] == mtime
 
 def _read_logs():
-	with open("logs.json", "r") as f:
-		return json.load(f)
+    with open("logs.json", "r") as f:
+        return json.load(f)
 
 def is_in_logs(dst):
-	return True if dst in _read_logs() else False
+    return True if dst in _read_logs() else False
 
 def change_log(dst, mtime):
-	dump = _read_logs()
-	dump[dst] = mtime
-	with open('logs.json', 'w') as f:
-		json.dump(dump, f)
+    dump = _read_logs()
+    dump[dst] = mtime
+    with open('logs.json', 'w') as f:
+        json.dump(dump, f)
 
 def create_logs(loglist, local_working_dir, remote_working_dir):
-	dump = dict()
-	for log in loglist:
-		log = os.path.join(remote_working_dir, log)
-		dump[log] = os.path.getmtime(log)
-	with open('logs.json', 'w') as f:
-		json.dump(dump, f)
+    dump = dict()
+    for log in loglist:
+        log = os.path.join(remote_working_dir, log)
+        dump[log] = os.path.getmtime(log)
+    with open('logs.json', 'w') as f:
+        json.dump(dump, f)
