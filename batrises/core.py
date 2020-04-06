c1f717b2890592b9d32170d5923e49f2cda5658e
persorkki <herroo@protonmail.com>
2020-04-06 15:43:04 +0300

    very basic functionality to overwrite the remote files even if they werent modified by us

--- batrises/core.py
+++ batrises/core.py
@@ -13,7 +13,7 @@ def check_dir_tree(dst_full_path):
         os.makedirs(dst_full_path)
 
 def do_path_pull(relative_path, file, src_working_dir, dst_working_dir):
-    ( src_full_path, dst_full_path, dst_path_no_file ) = get_paths(relative_path, file, src_working_dir, dst_working_dir)
+    ( src_full_path, dst_full_path, src_path_no_file, dst_path_no_file ) = get_paths(relative_path, file, src_working_dir, dst_working_dir)
     print (f"\n== PULL == ")
     display_src_dst(src_full_path, dst_full_path)
     if os.path.isfile(src_full_path):
@@ -47,12 +47,16 @@ def do_copy_op(src_full_path, dst_full_path, dst_path_no_file, is_Logged):
 def is_newer(a, b):
     return True if os.path.getmtime(a) > os.path.getmtime(b) else False
 
-def get_paths(r, f, s, d):
-    #TODO: lol
-    return (os.path.join(s, r, f), os.path.join(d, r, f), os.path.join(d, r))
+def get_paths(relative_path, file, src_working_dir, dst_working_dir):
+    return (
+        os.path.join(src_working_dir, relative_path, file),
+        os.path.join(dst_working_dir, relative_path, file),
+        os.path.join(src_working_dir, relative_path),  
+        os.path.join(dst_working_dir, relative_path),
+        )
 
 def do_path_push(relative_path, file, src_working_dir, dst_working_dir):
-    ( src_full_path, dst_full_path, dst_path_no_file ) = get_paths(relative_path, file, src_working_dir, dst_working_dir)
+    ( src_full_path, dst_full_path, src_path_no_file, dst_path_no_file ) = get_paths(relative_path, file, src_working_dir, dst_working_dir)
     
     # TODO: TRY-CATCH for file checks?
     # step-by-step for this mess:
@@ -73,7 +77,23 @@ def do_path_push(relative_path, file, src_working_dir, dst_working_dir):
 
                 elif logs.is_in_logs(dst_full_path) and not logs.we_modified(dst_full_path, os.path.getmtime(dst_full_path)):
                 # we didnt modify it last but its in the logs
-                    print (f"[ LOG ]  DST was not last modified by this program")
+                    #print (f"[ LOG ]  DST was not last modified by this program")
+                    while(True):
+                        print ("DST was not modified by this program, how do you wish to proceed?")
+                        print ("1. Copy SRC to DST anyway, overwriting the DST file")
+                        print ("2. Copy DST to SRC, overwriting the local copy")
+                        print ("3. Ignore for now")
+                        x = input ("choice[1-3]: ")
+                        if x == "1":
+                            print ("copying SRC to DST")
+                            do_copy_op(src_full_path, dst_full_path, dst_path_no_file, True)
+                            break
+                        elif x == "2":
+                            print ("copying DST to SRC")
+                            do_copy_op(dst_full_path, src_full_path, src_path_no_file, False)
+                            break
+                        elif x == "3":
+                            break
                 else:
                 # file exists in destination but it isnt logged, do nothing
                     print (f"[ LOG ]  DST exists but is not logged, not pushing")
